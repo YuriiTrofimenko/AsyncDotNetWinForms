239eb6b7e79d52e1b0e703a8c7e89243458cbd85
YuriiTrofimenko <tyaa@ukr.net>
2020-03-19 20:56:04 +0200

    Добавьте файлы проекта.

---
+++ AsyncDotNetWinForms/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncDotNetWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            using (SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Disease;Integrated Security=True"))
+            {
+                conn.Open();
+                SqlCommand selectCommand = conn.CreateCommand();
+                selectCommand.CommandText = @"SELECT * FROM [Large_table_3]";
+                IAsyncResult asyncResult = selectCommand.BeginExecuteReader();
+                while (!asyncResult.IsCompleted)
+                {
+                    Console.Write(".");
+                    Thread.Sleep(500);
+                }
+                Console.WriteLine();
+                SqlDataReader reader =
+                    selectCommand.EndExecuteReader(asyncResult);
+                while (reader.Read())
+                {
+                    if (!((int)reader["id"] == 57863))
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.WriteLine(reader[i] + "    ");
+                        }
+                    }
+                    else
+                    {
+                        reader.Close();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,23 @@
+﻿--TRUNCATE TABLE Large_table_3;
+
+/*CREATE TABLE Large_table_3
+(
+	id INT NOT NULL IDENTITY(1, 1),
+	color VARCHAR(25) NULL,
+	size VARCHAR(25) NULL
+)*/
+GO
+DECLARE @i INT = 0
+DECLARE @var INT = 1;
+WHILE @i < 1000000
+	BEGIN
+		IF @i IN (999999, 999995, 999993)
+			SET @var = 999
+		ELSE SET @var = 1;
+		INSERT Large_table_3 (color, size) VALUES
+		(
+			'COLOR ' + CAST(@var AS VARCHAR(5)),
+			'SIZE '  + CAST(@var AS VARCHAR(5))
+		);
+		SET @i = @i + 1
+	END
